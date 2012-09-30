9f70acc764f7bb7e0815941ef617b51d4b48a966
Kassim Halloyta <kassim@newish.dk>
2012-09-30 02:23:47 +0200

    pagination project creation

---
+++ paginer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paginer::Application.config.secret_token = '523c1721b2444e0fd64ffe7c19a4da1cfd27c1f991cfd6c6afe198fb124067d55fcd0d089764d1ce403e36303206fc838af927adfe60bc0500e33c0b6d6793f3'
